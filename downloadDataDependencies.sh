c63b0928644d191bb13b860f055ba3fecdbecbdb
Jake Lever <jlever@bcgsc.ca>
2019-08-05 12:08:12 -0700

    Move download files into one

--- downloadDataDependencies.sh
+++ downloadDataDependencies.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ex
 
+wget ftp://ftp.ncbi.nih.gov/snp/latest_release/VCF/GCF_000001405.25.bgz
+
 wget ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/asciimesh/c2019.bin
 wget ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/asciimesh/d2019.bin
+
+wget ftp://ftp.ncbi.nlm.nih.gov/pub/lu/PubTatorCentral/bioconcepts2pubtatorcentral.gz
 
--- downloadPubTatorCentral.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ex
-
-wget ftp://ftp.ncbi.nlm.nih.gov/pub/lu/PubTatorCentral/bioconcepts2pubtatorcentral.gz
-
