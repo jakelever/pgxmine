898d863d75e8764816a547dcba40c8b8f8c4f64e
Jake Lever <jlever@bcgsc.ca>
2019-08-05 11:38:03 -0700

    Add download scripts

---
+++ downloadMeSH.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -ex
+
+wget ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/asciimesh/c2019.bin
+wget ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/asciimesh/d2019.bin
+
---
+++ downloadPubTatorCentral.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -ex
+
+wget ftp://ftp.ncbi.nlm.nih.gov/pub/lu/PubTatorCentral/bioconcepts2pubtatorcentral.gz
+
